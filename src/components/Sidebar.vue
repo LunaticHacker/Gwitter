b49d06aad4ab4023a174a507ca609538e25dafc9
LunaticHacker <lunatechnology007@gmail.com>
2020-10-28 09:33:06 -0500

    Added Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="sidebar">
+    <font-awesome-icon class="logo" :icon="['fab', 'twitter']" />
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'home']" />
+      <p>Home</p>
+    </div>
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'hashtag']" />
+      <p>Explore</p>
+    </div>
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'bell']" />
+      <p>Notifications</p>
+    </div>
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'envelope']" />
+      <p>Messages</p>
+    </div>
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'list']" />
+      <p>Lists</p>
+    </div>
+    <div class="item">
+      <font-awesome-icon class="icon" :icon="['fas', 'user']" />
+      <p>Profile</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+}
+.logo {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  color: #1da1f2;
+  margin: 1em;
+}
+.logo:hover {
+  background: lightblue;
+}
+.icon {
+  margin-top: 1em;
+  width: 1em;
+  height: 1em;
+}
+.item {
+  display: flex;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1em;
+  font-weight: bold;
+  justify-content: left;
+  width: max-content;
+}
+.item:hover {
+  color: #1da1f2;
+}
+p {
+  justify-content: space-around;
+  margin-left: 1em;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faHashtag,
+  faBookmark,
+  faBell,
+  faHome,
+  faEnvelope,
+  faEllipsisH,
+  faList,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-Vue.config.productionTip = false
+library.add(faTwitter);
+library.add({
+  faHashtag,
+  faBookmark,
+  faBell,
+  faHome,
+  faEnvelope,
+  faEllipsisH,
+  faList,
+  faUser,
+});
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
