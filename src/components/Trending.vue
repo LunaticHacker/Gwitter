ae830802706c969d6556bd22bc3bfe926be0cdb6
LunaticHacker <lunatechnology007@gmail.com>
2020-10-28 11:08:02 -0500

    Add searchBar

---
+++ src/components/Trending.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input class="searchbar" type="text" placeholder="Search Gwitter" />
+  </div>
+</template>
+<script>
+export default {
+  name: "Trending",
+};
+</script>
+
+<style scoped>
+.searchbar {
+  width: 80%;
+  height: 10px;
+  color: rgb(102, 102, 102);
+  background-color: rgb(204, 204, 204);
+  border: none;
+  border-radius: 13px 13px 13px 13px;
+  padding: 1em;
+  margin-right: 1em;
+}
+</style>
